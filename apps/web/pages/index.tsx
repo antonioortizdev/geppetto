3fefed092a7e9cb2cf344d7af3a62b1a0bb11121
Antonio Ortiz <antonioortizdev@gmail.com>
2023-03-25 20:10:22 +0100

    set form

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,25 @@
-import { Button } from 'ui';
-
 export default function Web() {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    const apiUrl: URL = new URL(process.env.NEXT_PUBLIC_API_URL as string);
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text }),
+    });
+  };
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
+    <main>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Your message:
+          <input type="text" name="text" />
+          <button>Send</button>
+        </label>
+      </form>
+    </main>
   );
 }
